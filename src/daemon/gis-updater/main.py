9071b51d8ae9c11a4ac2ece3dd9ac66ab6fd3866
diogo012 <79468459+diogo012@users.noreply.github.com>
2023-01-15 14:50:06 +0000

    Update main.py

--- src/daemon/gis-updater/main.py
+++ src/daemon/gis-updater/main.py
@@ -1,10 +1,39 @@
 import sys
 import time
 
+import psycopg2
+from psycopg2 import OperationalError
+
 POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 60
 ENTITIES_PER_ITERATION = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
 
+def get_coord():
+    """ query data from the coordenadas table """
+    
+    conn = psycopg2.connect(host='db-rel', database='is', user='is', password='is')
+    
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
 if __name__ == "__main__":
+    
+    
 
     while True:
         print(f"Getting up to {ENTITIES_PER_ITERATION} entities without coordinates...")
@@ -12,3 +41,5 @@ if __name__ == "__main__":
         # !TODO: 2- Use the entity information to retrieve coordinates from an external API
         # !TODO: 3- Submit the changes
         time.sleep(POLLING_FREQ)
+
+
